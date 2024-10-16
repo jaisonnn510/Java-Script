061b22dc91d388f90eca08a6540473ff038e0ca2
jaisonnn510 <jaison2004dsouza@gmail.com>
2024-10-16 15:17:01 +0000

    conversions and datatypes

---
+++ basics/conversion.js
@@ -0,0 +1,6 @@
+let value = "33"
+
+
+const newName = Number(value)
+console.log(typeof newName);
+console.log( newName);
---
+++ basics/datatypes.js
@@ -0,0 +1,8 @@
+let value = 33
+console.log(typeof value);
+
+let name = "Rohan"
+console.log(typeof name)
+
+let js  = true
+console.log(typeof js)
